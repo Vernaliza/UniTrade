eec5247eefd574e73f695a21ab4ea06ba58fc741
Vernaliza <98198046+Vernaliza@users.noreply.github.com>
2026-03-12 14:35:12 +0000

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-(qnk_v9u8h8@x5ou9ocmj!q&dwy*uc9z%4m36^1l)8-qeis2sz
 #DEBUG = False
 DEBUG = True
 
-ALLOWED_HOSTS = ["webtest.bearland.online","101.126.137.137","8.208.127.158","www.unitrade.world","unitrade.world",'*', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["webtest.bearland.online","8.208.126.103","101.126.137.137","8.208.127.158","www.unitrade.world","unitrade.world",'*', 'localhost', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = [
     "http://unitrade.world",
